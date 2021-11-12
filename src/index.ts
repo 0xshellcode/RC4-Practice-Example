9a3a8f5e057928c9d3f0ab418baef45415adfa22
0xshellcode <david.r0sas@protonmail.com>
2021-11-11 19:17:56 -0600

    Add RC4 implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log('Hello World');
+const plainText: any = 'mazacuata';
+const key: any = '12190415';
+
+// let cipherText = new ArrayBuffer(plainText.length);
+
+const cipherText = (plainText: any, key: any) => {
+  let finalResult = [];
+
+  // Key Scheduling Algorithm
+
+  let s_vector = [];
+  let expanded_key: any = [];
+
+  for (let i = 0; i <= 256; i++) {
+    expanded_key.push(key[i % key.length]);
+    s_vector.push(i);
+  }
+
+  let j = 0;
+  for (let i = 0; i <= 256; i++) {
+    j = j + s_vector[i] + (expanded_key[i] % 256);
+    [s_vector[i], s_vector[j]] = [s_vector[j], s_vector[i]];
+  }
+
+  // Pseudo Random Generation Algorithm
+
+  let i = 0;
+  j = 0;
+
+  let byte: any = 0;
+  for (byte in plainText) {
+    i = (i + 1) % 256;
+    j = (j + s_vector[i]) % 256;
+    s_vector[i], (s_vector[j] = s_vector[j]), s_vector[i];
+    let t = (s_vector[i] + s_vector[j]) % 256;
+    let k = s_vector[t];
+    finalResult.push(byte ^ k);
+  }
+  return finalResult;
+};
+
+console.log(cipherText(plainText, key));
