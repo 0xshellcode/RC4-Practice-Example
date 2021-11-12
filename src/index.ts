d572d1a93b88036d46cd8a6a6f3ba6f7b03536d3
0xshellcode <david.r0sas@protonmail.com>
2021-11-11 20:46:01 -0600

    Add ascii representation

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const encodedplainText = encoder.encode(plainText);
 console.log(`Encoded Key: ${encodedKey}`);
 console.log(`Encoded PlainText: ${encodedplainText}`);
 
-const cipherText = (plainText: any, key: any) => {
+const rc4Algorithm = (plainText: any, key: any) => {
   let finalResult = [];
 
   // Key Scheduling Algorithm
@@ -17,29 +17,23 @@ const cipherText = (plainText: any, key: any) => {
   let s_vector = [];
   let expanded_key: any = [];
 
-  for (let i = 0; i <= 256; i++) {
+  for (let i = 0; i < 256; i++) {
     expanded_key.push(encodedKey[i % encodedKey.length]);
     s_vector.push(i);
   }
 
-  console.log(s_vector);
-  console.log(expanded_key);
-
   let j = 0;
-  for (let i = 0; i <= 256; i++) {
+  for (let i = 0; i < 256; i++) {
     j = (j + s_vector[i] + expanded_key[i]) % 256;
     [s_vector[i], s_vector[j]] = [s_vector[j], s_vector[i]];
   }
-
-  console.log(j);
 
   // Pseudo Random Generation Algorithm
 
   let i = 0;
   j = 0;
 
-  let byte: any = 0;
-  for (byte in plainText) {
+  for (let byte of encodedplainText) {
     i = (i + 1) % 256;
     j = (j + s_vector[i]) % 256;
     [s_vector[i], s_vector[j]] = [s_vector[j], s_vector[i]];
@@ -50,4 +44,19 @@ const cipherText = (plainText: any, key: any) => {
   return finalResult;
 };
 
-console.log(cipherText(plainText, key));
+console.log(rc4Algorithm(plainText, key));
+const encryptedMessage = rc4Algorithm(plainText, key);
+
+encryptedMessage.forEach((value) => {
+  console.log(value.toString(16));
+});
+
+const hex2Ascii = (hexx: string) => {
+  let hex = hexx.toString(); //force conversion
+  let str = '';
+  for (let i = 0; i < hex.length; i += 2)
+    str += String.fromCharCode(parseInt(hex.substring(i, 2), 16));
+  return str;
+};
+
+console.log(hex2Ascii('81d4d216af11da'));
